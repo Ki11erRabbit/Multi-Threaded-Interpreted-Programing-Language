1393d55ef6d7d0be1715178715e3e231c27d5616
ki11errabbit <ki11errabbit@posteo.net>
2023-07-24 21:27:53 -0600

    we can now parse keywords, operators, and symbols

--- src/parser.rs
+++ src/parser.rs
@@ -62,30 +62,30 @@ fn keywords() -> impl Parser<char, Token, Error = Simple<char>> {
 
     let keyword = recursive(|kwd| {
         
-        let class = text::keyword::<_, _, Simple<char>>("class").map(|_| Token::Class);
-        let instance = text::keyword::<_, _, Simple<char>>("instance").map(|_| Token::Instance);
-        let default = text::keyword::<_, _, Simple<char>>("default").map(|_| Token::Default);
-        let sum = text::keyword::<_, _, Simple<char>>("sum").map(|_| Token::Sum);
-        let product = text::keyword::<_, _, Simple<char>>("product").map(|_| Token::Product);
-        let type_ = text::keyword::<_, _, Simple<char>>("type").map(|_| Token::Type);
-        let function = text::keyword::<_, _, Simple<char>>("fn").map(|_| Token::Function);
-        let match_ = text::keyword::<_, _, Simple<char>>("match").map(|_| Token::Match);
-        let while_ = text::keyword::<_, _, Simple<char>>("while").map(|_| Token::While);
-        let elwhile = text::keyword::<_, _, Simple<char>>("elwhile").map(|_| Token::ElWhile);
-        let for_ = text::keyword::<_, _, Simple<char>>("for").map(|_| Token::For);
-        let loop_ = text::keyword::<_, _, Simple<char>>("loop").map(|_| Token::Loop);
-        let if_ = text::keyword::<_, _, Simple<char>>("if").map(|_| Token::If);
-        let elif = text::keyword::<_, _, Simple<char>>("elif").map(|_| Token::Elif);
-        let else_ = text::keyword::<_, _, Simple<char>>("else").map(|_| Token::Else);
-        let continue_ = text::keyword::<_, _, Simple<char>>("continue").map(|_| Token::Continue);
-        let break_ = text::keyword::<_, _, Simple<char>>("break").map(|_| Token::Break);
-        let in_ = text::keyword::<_, _, Simple<char>>("in").map(|_| Token::In);
-        let typeis = text::keyword::<_, _, Simple<char>>("typeis").map(|_| Token::Typeis);
-        let effect = text::keyword::<_, _, Simple<char>>("effect").map(|_| Token::Effect);
-        let with = text::keyword::<_, _, Simple<char>>("with").map(|_| Token::With);
-        let return_ = text::keyword::<_, _, Simple<char>>("return").map(|_| Token::Return);
-        let mod_ = text::keyword::<_, _, Simple<char>>("mod").map(|_| Token::Mod);
-        let import = text::keyword::<_, _, Simple<char>>("import").map(|_| Token::Import);
+        let class = text::keyword::<_, _, Simple<char>>("class").map(|_| Token::Class).padded();
+        let instance = text::keyword::<_, _, Simple<char>>("instance").map(|_| Token::Instance).padded();
+        let default = text::keyword::<_, _, Simple<char>>("default").map(|_| Token::Default).padded();
+        let sum = text::keyword::<_, _, Simple<char>>("sum").map(|_| Token::Sum).padded();
+        let product = text::keyword::<_, _, Simple<char>>("product").map(|_| Token::Product).padded();
+        let type_ = text::keyword::<_, _, Simple<char>>("type").map(|_| Token::Type).padded();
+        let function = text::keyword::<_, _, Simple<char>>("fn").map(|_| Token::Function).padded();
+        let match_ = text::keyword::<_, _, Simple<char>>("match").map(|_| Token::Match).padded();
+        let while_ = text::keyword::<_, _, Simple<char>>("while").map(|_| Token::While).padded();
+        let elwhile = text::keyword::<_, _, Simple<char>>("elwhile").map(|_| Token::ElWhile).padded();
+        let for_ = text::keyword::<_, _, Simple<char>>("for").map(|_| Token::For).padded();
+        let loop_ = text::keyword::<_, _, Simple<char>>("loop").map(|_| Token::Loop).padded();
+        let if_ = text::keyword::<_, _, Simple<char>>("if").map(|_| Token::If).padded();
+        let elif = text::keyword::<_, _, Simple<char>>("elif").map(|_| Token::Elif).padded();
+        let else_ = text::keyword::<_, _, Simple<char>>("else").map(|_| Token::Else).padded();
+        let continue_ = text::keyword::<_, _, Simple<char>>("continue").map(|_| Token::Continue).padded();
+        let break_ = text::keyword::<_, _, Simple<char>>("break").map(|_| Token::Break).padded();
+        let in_ = text::keyword::<_, _, Simple<char>>("in").map(|_| Token::In).padded();
+        let typeis = text::keyword::<_, _, Simple<char>>("typeis").map(|_| Token::Typeis).padded();
+        let effect = text::keyword::<_, _, Simple<char>>("effect").map(|_| Token::Effect).padded();
+        let with = text::keyword::<_, _, Simple<char>>("with").map(|_| Token::With).padded();
+        let return_ = text::keyword::<_, _, Simple<char>>("return").map(|_| Token::Return).padded();
+        let mod_ = text::keyword::<_, _, Simple<char>>("mod").map(|_| Token::Mod).padded();
+        let import = text::keyword::<_, _, Simple<char>>("import").map(|_| Token::Import).padded();
 
         class
             .or(instance)
@@ -137,7 +137,7 @@ mod keywords_tests {
     }
     #[test]
     fn test_instance() {
-        let result = keywords().parse("instance");
+        let result = keywords().parse(" instance");
 
         if result.is_err() {
             eprintln!("{:?}", result);
@@ -151,7 +151,7 @@ mod keywords_tests {
 
     #[test]
     fn test_return() {
-        let result = keywords().parse("return");
+        let result = keywords().parse("return ");
 
         if result.is_err() {
             eprintln!("{:?}", result);
@@ -172,11 +172,11 @@ fn operators() -> impl Parser<char, Token, Error = Simple<char>> {
     let operator = recursive(|op| {
         choice((
             just("=").to(Token::Assignment).padded(),
-            just(":").to(Token::Colon),
+            just(":").to(Token::Colon).padded(),
             just(".").to(Token::Period),
-            just(":=").to(Token::MutableAssignment),
-            just("@").to(Token::Attribute),
-            just("_").to(Token::WildCard),
+            just(":=").to(Token::MutableAssignment).padded(),
+            just("@").to(Token::Attribute).padded(),
+            just("_").to(Token::WildCard).padded(),
             ))
     });
 
@@ -206,7 +206,7 @@ mod operator_tests {
 
     #[test]
     fn test_colon() {
-        let result = operators().parse(":");
+        let result = operators().parse(": ");
 
         if result.is_err() {
             eprintln!("{:?}", result);
@@ -232,6 +232,68 @@ mod operator_tests {
         assert_eq!(token, Token::Period, "Token not period");
     }
 
+    #[test]
+    fn test_period_fail() {
+        let result = operators().parse(" .");
+
+        if result.is_ok() {
+            eprintln!("{:?}", result);
+            assert!(false, "Parser should have failed");
+        }
+    }
+}
+
+
+fn symbols() -> impl Parser<char, Token, Error = Simple<char>> {
+
+    let symbol = recursive(|sym| {
+        choice((
+            just("[").to(Token::BracketLeft).padded(),
+            just("]").to(Token::BracketRight).padded(),
+            just("(").to(Token::ParenLeft).padded(),
+            just(")").to(Token::ParenRight).padded(),
+            just("{").to(Token::CurlyLeft).padded(),
+            just("}").to(Token::CurlyRight).padded(),
+            just(",").to(Token::Comma).padded(),
+            just(";").to(Token::Semicolon).padded(),
+            just("->").to(Token::FunctionReturn).padded(),
+            just("=>").to(Token::MatchArm).padded(),
+            just("::").to(Token::Namespace),
+            ))
+    });
+
+    symbol
+}
+
+
+#[cfg(test)]
+mod symbol_tests {
+    use super::*;
+    use chumsky::prelude::*;
+
+    #[test]
+    fn test_bracket() {
+        let result = symbols().parse("[");
+
+        if result.is_err() {
+            eprintln!("{:?}", result);
+            assert!(false, "Error parsing bracket");
+        }
+
+        let token = result.unwrap();
+
+        assert_eq!(token, Token::BracketLeft, "Token not bracket");
+    }
+
+    #[test]
+    fn test_namespace_fail() {
+        let result = symbols().parse(" ::");
+
+        if result.is_ok() {
+            eprintln!("{:?}", result);
+            assert!(false, "Parser should have failed");
+        }
+    }
 
 }
 
