187df61a84c08873589749cfbb669f2f1e46fb0f
ki11errabbit <ki11errabbit@posteo.net>
2023-07-29 10:40:45 -0600

    implemented sum type parser

---
+++ src/parser/algabraic_type_parser.rs
@@ -0,0 +1,141 @@
+use chumsky::prelude::*;
+
+use crate::parser::lexer::Token;
+use crate::parser::type_parser::{type_parser};
+
+use crate::types::Type;
+
+
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct SumType {
+    name: Type,
+    variants: Vec<(String, Option<Type>)>,
+}
+
+
+
+
+fn sum_type_parser() -> impl Parser<Token, SumType, Error = Simple<Token>> {
+
+
+    let variant_parser =
+        filter_map(|span, token| match token {
+            Token::Identifier(name) => Ok(name),
+            _ => Err(Simple::custom(span, "Expected identifier".to_string())),
+        })
+        .then(type_parser())
+            .map(|(name, typ)| (name, Some(typ)))
+            .or(
+                filter_map(|span, token| match token {
+                    Token::Identifier(name) => Ok(name),
+                    _ => Err(Simple::custom(span, "Expected identifier".to_string())),
+                })
+                .map(|name| (name, None))
+            )
+            .labelled("variant");
+
+
+    let sum_type = just(Token::Sum)
+        .ignore_then(type_parser())
+        .then_ignore(just(Token::CurlyLeft))
+        .then(variant_parser
+              .separated_by(just(Token::Comma)))
+        .then_ignore(just(Token::CurlyRight))
+        .map(|(name, variants)| SumType { name, variants })
+        .labelled("sum type");
+
+    sum_type
+}
+
+
+#[cfg(test)]
+mod sum_type_tests {
+    use super::*;
+    use crate::parser::lexer::lexer;
+
+    #[test]
+    fn sum_type_test() {
+        let input = "sum type a { A, B, C }";
+
+        let lexer_result = lexer(input);
+
+        if lexer_result.is_err() {
+            assert!(false,"Lexer error: {:?}", lexer_result.err());
+        }
+
+        let tokens = lexer_result.unwrap();
+
+        let result = sum_type_parser().parse(tokens);
+
+        if result.is_err() {
+            assert!(false,"Parser error: {:?}", result.err());
+        }
+
+        let sum_type = result.unwrap();
+
+        assert_eq!(sum_type.name, Type::Single("a".to_string()), "Name is not correct");
+        assert_eq!(sum_type.variants.len(), 3, "Number of variants is not correct");
+        assert_eq!(sum_type.variants[0].0, "A".to_string(), "Variant is not A");
+        assert_eq!(sum_type.variants[1].0, "B".to_string(), "Variant is not B");
+        assert_eq!(sum_type.variants[2].0, "C".to_string(), "Variant is not C");
+    }
+
+    #[test]
+    fn sum_type_with_type_test() {
+        let input = "sum type a { A, B, C(Int) }";
+
+        let lexer_result = lexer(input);
+
+        if lexer_result.is_err() {
+            assert!(false,"Lexer error: {:?}", lexer_result.err());
+        }
+
+        let tokens = lexer_result.unwrap();
+
+        let result = sum_type_parser().parse(tokens.clone());
+
+        if result.is_err() {
+            eprintln!("Tokens: {:?}", tokens);
+            assert!(false,"Parser error: {:?}", result.err());
+        }
+
+        let sum_type = result.unwrap();
+
+        assert_eq!(sum_type.name, Type::Single("a".to_string()), "Name is not correct");
+        assert_eq!(sum_type.variants.len(), 3, "Number of variants is not correct");
+        assert_eq!(sum_type.variants[0].0, "A".to_string(), "Variant is not A");
+        assert_eq!(sum_type.variants[1].0, "B".to_string(), "Variant is not B");
+        assert_eq!(sum_type.variants[2].0, "C".to_string(), "Variant is not C");
+        assert_eq!(sum_type.variants[2].1, Some(Type::Tuple(vec![Type::Single("Int".to_string())])), "Variant C does not hava a Tuple of Int");
+    }
+
+    #[test]
+    fn test_maybe_type() {
+        let input = "sum type (Maybe a) { Just(a), Nothing }";
+
+        let lexer_result = lexer(input);
+
+        if lexer_result.is_err() {
+            assert!(false,"Lexer error: {:?}", lexer_result.err());
+        }
+
+        let tokens = lexer_result.unwrap();
+
+        let result = sum_type_parser().parse(tokens.clone());
+
+        if result.is_err() {
+            eprintln!("Tokens: {:?}", tokens);
+            assert!(false,"Parser error: {:?}", result.err());
+        }
+
+        let sum_type = result.unwrap();
+
+        assert_eq!(sum_type.name, Type::TypeList{ name: Box::new(Type::Single("Maybe".to_string())), parameters: vec![Type::Single("a".to_string())] }, "Name is not correct");
+        assert_eq!(sum_type.variants.len(), 2, "Number of variants is not correct");
+        assert_eq!(sum_type.variants[0].0, "Just".to_string(), "Variant is not Just");
+        assert_eq!(sum_type.variants[1].0, "Nothing".to_string(), "Variant is not Nothing");
+
+    }
+
+}
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -2,6 +2,7 @@ pub mod lexer;
 pub mod ast;
 pub mod type_parser;
 pub mod type_class_parser;
+pub mod algabraic_type_parser;
 
 
 
